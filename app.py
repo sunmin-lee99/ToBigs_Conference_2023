c2d06379534b82ce3b78e7c4fe40b402add0e3d6
dlduddk <49145450+dlduddk@users.noreply.github.com>
2023-07-08 15:37:07 +0900

    app_blip

---
+++ app.py
@@ -0,0 +1,20 @@
+# pip3 install streamlit
+
+import streamlit as st
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+uploaded_file = st.file_uploader("Upload Image")
+
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+
+raw_image = Image.open(uploaded_file).convert('RGB')
+# conditional image captioning
+text = "a drawing of"
+inputs = processor(raw_image, text, return_tensors="pt")
+
+out = model.generate(**inputs)
+#print(processor.decode(out[0], skip_special_tokens=True))
+
+st.image(raw_image, caption=processor.decode(out[0], skip_special_tokens=True))
